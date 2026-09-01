28015bb30519b22f9d614ef3bd6f013664d13337
Yamzz-Official <ryanrmdn207@gmail.com>
2026-09-02 03:26:35 +0700

    Delete public directory

--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Vercel PHP entrypoint. Requires a PostgreSQL DATABASE_URL environment variable.
-require_once __DIR__ . '/../src/bootstrap.php';
-require_once __DIR__ . '/../src/router.php';
